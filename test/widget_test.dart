de13d31bfff5c537366057910f90c5764833d94f
johnas20 <johnas@uninorte.edu.co>
2021-12-21 06:08:39 -0500

    ORganizar test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:auth_firebase_app/main.dart';
+import 'package:auth_firebase_app/ui/my_app.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
